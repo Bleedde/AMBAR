2f9d298b092595f3a43373d0e60406005674979e
Bleedde <ttreyygh@gmail.com>
2025-11-11 16:41:34 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+extend: {
+  animation: {
+    marquee: "marquee 25s linear infinite",
+  },
+  keyframes: {
+    marquee: {
+      "0%": { transform: "translateX(0)" },
+      "100%": { transform: "translateX(-50%)" },
+    },
+  },
+},
+  },
+  plugins: [],
+};
